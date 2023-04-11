a5c94b191e6e4a0d12affcaf8507e3a9cbd53327
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-11 16:43:51 +0200

    Update lab06.py

--- lab06.py
+++ lab06.py
@@ -5,10 +5,10 @@ import streamlit as st
 
 translator = Translator()
 word_user=st.text_input('Give some text:')
+language='de'
+languageis = translator.translate(word_user, dest=language)
 
-languageis = translator.translate(word_user, dest='de')
-
-tts=gTTS(text=languageis.text, lang='de')
+tts=gTTS(text=languageis.text, lang=language)
 tts.save('user.mp3')
 print('in german:')
 st.audio('user.mp3')
