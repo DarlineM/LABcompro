64ea49f8f60d2663d2b15fd08d813d9a0bb2339d
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-11 15:34:28 +0200

    Update lab05.py

--- lab05.py
+++ lab05.py
@@ -2,7 +2,7 @@ import json, requests
 from googletrans import Translator
 import streamlit as st
 
-language = st.selectbox('Please, select destination language')
+language = st.selectbox('Please, select destination language', ('it', 'de', 'en', 'fr'))
 keyword = st.text_input('Insert a word')
 translator = Translator()
 
