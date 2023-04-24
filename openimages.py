201f31987ec9bbf3faa48f16ac7837b07e834b31
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-24 15:39:15 +0200

    Update openimages.py

--- openimages.py
+++ openimages.py
@@ -6,12 +6,9 @@ import random
 
 #st.image(img)
 
-items = ['to_eat', 'to_read', 'tree', 'apple']
-
-rand_item = random.choice(items)
-
-picture = rand_item + '.jpg'
-
-img = Image.open(picture)
-
-st.image(img)
+if st.start_button(label='START'):
+  items = ['to_eat', 'to_read', 'tree', 'apple']
+  rand_item = random.choice(items)
+  picture = rand_item + '.jpg'
+  img = Image.open(picture)
+  st.image(img)
