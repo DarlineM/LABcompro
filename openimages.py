aa44ccd68a5079313b82cf0d1589e9f1ecded715
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-24 15:28:34 +0200

    Update openimages.py

--- openimages.py
+++ openimages.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from PIL import Image
+import random
 
-img = Image.open('apple.jpg')
+#img = Image.open('apple.jpg')
+
+#st.image(img)
+
+items = ['to eat', 'to read', 'summer', 'winter', 'tree', 'head', 'ear', 'apple']
+
+rand_item = random.choice(items)
+
+picture = rand_item + '.jpg'
+
+img = Image.open(picture)
 
 st.image(img)
