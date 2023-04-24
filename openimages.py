4ad1586959510914f9004f49900bd34de2dfcaf2
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-24 16:07:06 +0200

    Update openimages.py

--- openimages.py
+++ openimages.py
@@ -6,31 +6,36 @@ import random
 #img = Image.open('apple.jpg')
 
 #st.image(img)
-#items = ['to_eat', 'to_read', 'tree', 'apple']
-#rand_item = random.choice(items)
 
-if st.button(label='START'):
+counter = 1
+while (counter <= 1):
   items = ['to_eat', 'to_read', 'tree', 'apple']
   rand_item = random.choice(items)
+  
+  if st.button(label='START'):
+  #items = ['to_eat', 'to_read', 'tree', 'apple']
+  #rand_item = random.choice(items)
   picture = rand_item + '.jpg'
   img = Image.open(picture)
   st.image(img)
   
-keyword = rand_item
-option = st.selectbox("Choose one",("It is another word for", "It sounds like", 
+  keyword = rand_item
+  option = st.selectbox("Choose one",("It is another word for", "It sounds like", 
                                     "It means like", "It rhymes with"))
-if option:
-  if option == "It is another word for":
-    key ="rel_syn"
-  elif option == "It sounds like":
-    key = "sl"
-  elif option == "It means like":
-    key = "ml"
-  elif option == "It rhymes with":
-    key = "rhy"
+  if option:
+    if option == "It is another word for":
+      key ="rel_syn"
+    elif option == "It sounds like":
+      key = "sl"
+    elif option == "It means like":
+      key = "ml"
+    elif option == "It rhymes with":
+      key = "rhy"
       
-  if(key and keyword):
-    url= 'https://api.datamuse.com/words?' + key + "=" + keyword 
-    response = requests.get(url)
-    dataFromDatamuse = json.loads(response.text) 
-    st.write(dataFromDatamuse)
+    if(key and keyword):
+      url= 'https://api.datamuse.com/words?' + key + "=" + keyword 
+      response = requests.get(url)
+      dataFromDatamuse = json.loads(response.text) 
+      st.write(dataFromDatamuse)
+      
+      counter = counter + 1
