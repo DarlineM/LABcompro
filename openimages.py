e1a390a8492a9d29cad497c714765c062c2f8e3d
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-24 15:59:52 +0200

    Update openimages.py

--- openimages.py
+++ openimages.py
@@ -6,12 +6,12 @@ import random
 #img = Image.open('apple.jpg')
 
 #st.image(img)
-items = ['to_eat', 'to_read', 'tree', 'apple']
-rand_item = random.choice(items)
+#items = ['to_eat', 'to_read', 'tree', 'apple']
+#rand_item = random.choice(items)
 
 if st.button(label='START'):
-  #items = ['to_eat', 'to_read', 'tree', 'apple']
-  #rand_item = random.choice(items)
+  items = ['to_eat', 'to_read', 'tree', 'apple']
+  rand_item = random.choice(items)
   picture = rand_item + '.jpg'
   img = Image.open(picture)
   st.image(img)
