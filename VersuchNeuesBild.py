5094b7bb1566eac11293f4c4f09d4d26e9876f6e
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 16:05:49 +0200

    Update VersuchNeuesBild.py

--- VersuchNeuesBild.py
+++ VersuchNeuesBild.py
@@ -8,6 +8,10 @@ i = 0
 while i < length:
     st.write("test", items[i])
     user_input2 = st.text_input("Try again now! Enter the word")
+    def clear_text():
+      st.session_state["text"] = ""
+    st.button("clear text input", on_click=clear_text)
+    st.write(input)
     if user_input2.lower() == items[i]:
       st.write("Super! Now you entered the correct word!")
       i += 1
