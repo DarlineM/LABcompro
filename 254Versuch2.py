ce39cf3c5a31e7428a3486053b964361c2797b8a
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 10:49:51 +0200

    Update 254Versuch2.py

--- 254Versuch2.py
+++ 254Versuch2.py
@@ -34,4 +34,10 @@ if option:
     dataFromDatamuse = json.loads(response.text)
     st.write(dataFromDatamuse)
     
+    st.write("\n")
+    st.write("\n")
+    user_input = st.text_input("Enter the word")
+    if user_input.lower() == str(rand_item):
+        st.write("You entered the correct word!")
+    
 
