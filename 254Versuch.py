8d4aa837d5df16ce3ec5b1eb67e3a65fe74b7c29
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 08:18:12 +0200

    Update 254Versuch.py

--- 254Versuch.py
+++ 254Versuch.py
@@ -37,8 +37,8 @@ if option:
  elif option == "It rhymes with":
   key = "rhy"
 
-if(key and keyword):
-  url= 'https://api.datamuse.com/words?' + key + "=" + keyword
-  response = requests.get(url)
-  dataFromDatamuse = json.loads(response.text)
-  st.write(dataFromDatamuse)
+  if(key and keyword):
+    url= 'https://api.datamuse.com/words?' + key + "=" + keyword
+    response = requests.get(url)
+    dataFromDatamuse = json.loads(response.text)
+    st.write(dataFromDatamuse)
