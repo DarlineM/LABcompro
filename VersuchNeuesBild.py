ddfad7952ff96cc38f287807ce2ff9de39751021
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 15:39:33 +0200

    Create VersuchNeuesBild.py

---
+++ VersuchNeuesBild.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+items = ['to_eat', 'to_read', 'tree', 'apple']
+
+length = len(items)
+i = 0
+
+while i < length:
+    st.write("test", items[i])
+    user_input2 = st.text_input("Try again now! Enter the word")
+    if user_input2.lower() == items[i]:
+      st.write("Super! Now you entered the correct word!")
+      i += 1
