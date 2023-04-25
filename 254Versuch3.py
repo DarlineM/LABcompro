70b687703d3077dad8c1af7bd4bd42a622034823
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 16:30:48 +0200

    Update 254Versuch3.py

--- 254Versuch3.py
+++ 254Versuch3.py
@@ -29,29 +29,29 @@ if user_input.lower() == str(rand_item):
 #elif user_input.lower() != str(rand_item):
     #st.write("Incorrect word. Please try again or get a hint.")
   
-  st.write("\n")
-  st.write("\n") 
-  st.subheader("Here you find help")
-  option = st.selectbox("Choose a hint", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
+st.write("\n")
+st.write("\n") 
+st.subheader("Here you find help")
+option = st.selectbox("Choose a hint", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
 
-  if option:
-    key_dict = {"It is another word for": "rel_syn", "It sounds like": "sl", "Similar in meaning to": "ml", "It rhymes with": "rel_rhy"}
-    key = key_dict[option] if option in key_dict else None
+if option:
+  key_dict = {"It is another word for": "rel_syn", "It sounds like": "sl", "Similar in meaning to": "ml", "It rhymes with": "rel_rhy"}
+  key = key_dict[option] if option in key_dict else None
 
-    if key:
-      keyword = rand_item
-      url = 'https://api.datamuse.com/words?' + key + "=" + keyword
-      response = requests.get(url)
-      dataFromDatamuse = json.loads(response.text)
-      st.write(dataFromDatamuse[0]["word"])
+  if key:
+    keyword = rand_item
+    url = 'https://api.datamuse.com/words?' + key + "=" + keyword
+    response = requests.get(url)
+    dataFromDatamuse = json.loads(response.text)
+    st.write(dataFromDatamuse[0]["word"])
     
-      st.write("\n")
-      st.write("\n")
-      user_input2 = st.text_input("Try again now! Enter the word")
-       if user_input2.lower() == str(rand_item):
-          st.subheader("Super! Now you entered the correct word!")
+    st.write("\n")
+    st.write("\n")
+    user_input2 = st.text_input("Try again now! Enter the word")
+     if user_input2.lower() == str(rand_item):
+        st.subheader("Super! Now you entered the correct word!")
           #counter += 1
-          st.write("\n")
-          st.write("\n")
-          st.header("If you want to continue practicing, press the START button above again")
-          break 
+        st.write("\n")
+        st.write("\n")
+        st.header("If you want to continue practicing, press the START button above again")
+        break 
