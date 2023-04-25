58f8c98aedc00d192ed4837196415af720c5b6a6
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-04-25 08:07:53 +0200

    Create 254Versuch.py

---
+++ 254Versuch.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import json,requests
+from PIL import Image
+import random
+
+img = Image.open('images/brainstorm.jpg')
+st.image(img)
+
+items = ['to_eat', 'to_read', 'tree', 'apple']
+rand_item = random.choice(items)
+
+picture = "images/" + rand_item + '.jpg'
+img = Image.open(picture)
+st.image(img, width = 300)
+
+st.write("\n")
+st.write("\n")
+
+option = st.multiselect("Choose one",["It is another word for", "It sounds like",
+"Similar in meaning to", "It rhymes with"])
+
+if option == []:
+st.stop()
+
+key_dict = {"It is another word for": "rel_syn",
+"It sounds like": "sl",
+"Similar in meaning to": "ml",
+"It rhymes with": "rel_rhy"}
+
+if option:
+key = key_dict[option[0]]
+
+if(key and keyword):
+url= 'https://api.datamuse.com/words?' + key + "=" + keyword
+response = requests.get(url)
+dataFromDatamuse = json.loads(response.text)
+st.write(dataFromDatamuse)
