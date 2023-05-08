058f6732c3b9f937c4699107d2df5d62017fce69
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-05-08 16:41:40 +0200

    Update 254Versuch2.py

--- 254Versuch2.py
+++ 254Versuch2.py
@@ -16,6 +16,7 @@ if st.button(label='START'):
     user_input = st.text_input("Enter the word")
     
     if user_input:
+        st.write(user_input)
         if user_input.lower() == str(rand_item):
             st.write("You entered the correct word!")
         else:
