50c23c0f416014144ea8d23f79dbde228d6bbd4d
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-05-12 09:19:59 +0200

    Update 1205Versuch.py

--- 1205Versuch.py
+++ 1205Versuch.py
@@ -31,22 +31,22 @@ if st.button(label='START'):
         st.write("\n")
         option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
 
-            if option:
-                 key_dict = {"It is another word for": "rel_syn", "It sounds like": "sl", "Similar in meaning to": "ml", "It rhymes with": "rel_rhy"}
-                key = key_dict[option] if option in key_dict else None
+    if option:
+      key_dict = {"It is another word for": "rel_syn", "It sounds like": "sl", "Similar in meaning to": "ml", "It rhymes with": "rel_rhy"}
+      key = key_dict[option] if option in key_dict else None
 
-                if key:
-                    keyword = rand_item
-                    url = 'https://api.datamuse.com/words?' + key + "=" + keyword
-                    response = requests.get(url)
-                    dataFromDatamuse = json.loads(response.text)
-                    st.write(dataFromDatamuse[0]["word"])
+      if key:
+        keyword = rand_item
+        url = 'https://api.datamuse.com/words?' + key + "=" + keyword
+        response = requests.get(url)
+        dataFromDatamuse = json.loads(response.text)
+        st.write(dataFromDatamuse[0]["word"])
     
-                    st.write("\n")
-                    st.write("\n")
-                    user_input2 = st.text_input("Try again now! Enter the word")
-                    if user_input2.lower() == str(rand_item):
-                        st.write("Super! Now you entered the correct word!")
+        st.write("\n")
+        st.write("\n")
+        user_input2 = st.text_input("Try again now! Enter the word")
+        if user_input2.lower() == str(rand_item):
+          st.write("Super! Now you entered the correct word!")
                 
                 
                 
