723fc0ed751d2e8b8295391fc40b945c1b07f2a8
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-05-17 12:22:43 +0200

    Update 175Versuch.py

--- 175Versuch.py
+++ 175Versuch.py
@@ -74,7 +74,7 @@ else:
 
 if st.button("Reload app"):
      #for key in st.session_state.keys():
-     del st.session_state[item]
+     session.RemoveAll()
      #st.write(st.session_state['item'])   
    
 
