944884260ded5f3270b032e9836006ceadfefe07
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-06-02 12:54:54 +0200

    Update 0206audio.py

--- 0206audio.py
+++ 0206audio.py
@@ -25,14 +25,14 @@ img = Image.open(picture)
 st.image(img, width=300)
 
 
-user_input = st.text_input("Enter the word")
+user_input = st.text_input("Enter the word here")
     
 if user_input:
     st.write("You entered:",user_input)
     if user_input.lower() == str(rand_item):
-        st.write("You entered the correct word!")
+        st.write("Congratulations! You entered the correct word!")
     else:
-        st.write("Unfortunately ths is incorrect. Please try again or get a hint below.")
+        st.write("Unfortunately this is incorrect. Please try again or get a hint below.")
         st.write("\n")
         st.write("\n")
         option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
@@ -58,11 +58,12 @@ if user_input:
                         st.write("Super! Now you entered the correct word!")
                     else:
                         st.write("Incorrect again. The word starts with", rand_item[0])
-                        user_input3 = st.text_input("Last chance, enter the word")
+                        user_input3 = st.text_input("Last chance, enter the word here")
                         if user_input3:
                           st.write("You entered:",user_input3)
                           if user_input3.lower() == str(rand_item):
                                         st.write("Super! Now you entered the correct word!")
+                                        st.write("Finally, let us speak the word together. Please press the play button below")
                                         tts=gTTS(text= rand_item, lang='en')
                                         tts.save('user.mp3')
                                         st.audio('user.mp3')
