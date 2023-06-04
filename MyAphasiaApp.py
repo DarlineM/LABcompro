79599de3ce6e313b12f20c381cfc4466f75f18cb
DarlineM <127765796+DarlineM@users.noreply.github.com>
2023-06-04 08:55:32 +0200

    Update MyAphasiaApp.py

--- MyAphasiaApp.py
+++ MyAphasiaApp.py
@@ -31,10 +31,11 @@ if st.session_state.page == 0:
     st.header("What do you see on the picture below?")
     st.write("\n")
     st.write("\n")
-    st.session_state.user_input = st.text_input("Enter the word and press NEXT")
+    #st.session_state.user_input = st.text_input("Enter the word and press NEXT")
   picture = "images/" + st.session_state.rand_item + '.jpg'
   img = Image.open(picture)
   st.image(img, width=300)
+  st.session_state.user_input = st.text_input("Enter the word and press NEXT")
 
 elif st.session_state.page == 1:
   if st.session_state.user_input:
@@ -67,20 +68,20 @@ elif st.session_state.page == 1:
                 st.write("\n")
                 st.session_state.user_input2 = st.text_input("Or try again and enter the word")
                 if st.session_state.user_input2:
-                    st.write("You entered:",st.session_state.user_input2)
+                    #st.write("You entered:",st.session_state.user_input2)
                     if st.session_state.user_input2.lower() == str(st.session_state.rand_item):
-                        st.write("Super! Now you entered the correct word!")
+                        st.write("Super!" "You entered:",st.session_state.user_input2, "This is the correct word!")
                         st.write("Finally, let us speak the word together. Please press the play button below")
                         tts=gTTS(text= st.session_state.rand_item, lang='en')
                         tts.save('user.mp3')
                         st.audio('user.mp3')
                     else:
-                        st.write("Incorrect again. The word starts with", st.session_state.rand_item[0])
+                        st.write("Unfortunately, this is incorrect again. The word starts with the letter", st.session_state.rand_item[0])
                         st.session_state.user_input3 = st.text_input("This is the last chance, enter the word here")
                         if st.session_state.user_input3:
-                          st.write("You entered:",st.session_state.user_input3)
+                          #st.write("You entered:",st.session_state.user_input3)
                           if st.session_state.user_input3.lower() == str(st.session_state.rand_item):
-                                        st.write("Super! Now you entered the correct word!")
+                                        st.write("Super! You entered:",st.session_state.user_input3, "This is the correct word!")
                                         st.write("Finally, let us speak the word together. Please press the play button below")
                                         tts=gTTS(text= st.session_state.rand_item, lang='en')
                                         tts.save('user.mp3')
